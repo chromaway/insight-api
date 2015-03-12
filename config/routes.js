f00dccd323053c4e010658e4f307334582ea8049
Kirill Fomichev <fanatid@ya.ru>
2015-03-12 14:34:33 +0300

    comment unused methods

--- config/routes.js
+++ config/routes.js
@@ -9,9 +9,9 @@ module.exports = function(app) {
 
   var apiPrefix = config.apiPrefix;
 
-  //Block routes
+  // Block routes
   var blocks = require('../app/controllers/blocks');
-  app.get(apiPrefix + '/blocks', blocks.list);
+  // app.get(apiPrefix + '/blocks', blocks.list);
   app.get(apiPrefix + '/headers', blocks.rawHeaders);
 
   app.get(apiPrefix + '/block/:blockHash', blocks.show);
@@ -22,9 +22,9 @@ module.exports = function(app) {
 
   // Transaction routes
   var transactions = require('../app/controllers/transactions');
-  app.get(apiPrefix + '/tx/:txid', transactions.show);
+  // app.get(apiPrefix + '/tx/:txid', transactions.show);
   app.get(apiPrefix + '/tx/:txid/hex', transactions.getRaw); // get raw tx
-  app.get(apiPrefix + '/txs', transactions.list);
+  // app.get(apiPrefix + '/txs', transactions.list);
   app.post(apiPrefix + '/tx/send', transactions.send);
   // app.param('txid', transactions.transaction); middleware moved to transactions.show
 
@@ -32,16 +32,16 @@ module.exports = function(app) {
   var addresses = require('../app/controllers/addresses');
   app.get(apiPrefix + '/addr/:addr', addresses.show);
   app.get(apiPrefix + '/addr/:addr/utxo', addresses.utxo);
-  app.get(apiPrefix + '/addrs/:addrs/utxo', addresses.multiutxo);
-  app.post(apiPrefix + '/addrs/utxo', addresses.multiutxo);
-  app.get(apiPrefix + '/addrs/:addrs/txs', addresses.multitxs);
-  app.post(apiPrefix + '/addrs/txs', addresses.multitxs);
+  // app.get(apiPrefix + '/addrs/:addrs/utxo', addresses.multiutxo);
+  // app.post(apiPrefix + '/addrs/utxo', addresses.multiutxo);
+  // app.get(apiPrefix + '/addrs/:addrs/txs', addresses.multitxs);
+  // app.post(apiPrefix + '/addrs/txs', addresses.multitxs);
 
   // Address property routes
-  app.get(apiPrefix + '/addr/:addr/balance', addresses.balance);
-  app.get(apiPrefix + '/addr/:addr/totalReceived', addresses.totalReceived);
-  app.get(apiPrefix + '/addr/:addr/totalSent', addresses.totalSent);
-  app.get(apiPrefix + '/addr/:addr/unconfirmedBalance', addresses.unconfirmedBalance);
+  // app.get(apiPrefix + '/addr/:addr/balance', addresses.balance);
+  // app.get(apiPrefix + '/addr/:addr/totalReceived', addresses.totalReceived);
+  // app.get(apiPrefix + '/addr/:addr/totalSent', addresses.totalSent);
+  // app.get(apiPrefix + '/addr/:addr/unconfirmedBalance', addresses.unconfirmedBalance);
 
   // Status route
   var st = require('../app/controllers/status');
@@ -51,8 +51,8 @@ module.exports = function(app) {
   app.get(apiPrefix + '/peer', st.peer);
 
   // Currency
-  var currency = require('../app/controllers/currency');
-  app.get(apiPrefix + '/currency', currency.index);
+  // var currency = require('../app/controllers/currency');
+  // app.get(apiPrefix + '/currency', currency.index);
 
   // Email store plugin
   if (config.enableEmailstore) {
@@ -84,7 +84,7 @@ module.exports = function(app) {
   app.get(apiPrefix + '/messages/verify', messages.verify);
   app.post(apiPrefix + '/messages/verify', messages.verify);
 
-  //Home route
+  // Home route
   var index = require('../app/controllers/index');
   app.get(apiPrefix + '/version', index.version);
   app.get('*', index.render);
